351c566fba0211f619f085d8569acc368482d993
Dan Sylvain <dansylvain84@gmail.com>
2020-09-22 16:28:39 +0200

    first changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,16 +3,16 @@ import './Footer.css'
 
 export default () => (
   <div>
-    <h2 className="taCenter">
+    {/* <h2 className="taCenter">
       Follow us{' '}
       <a href="https://instagram.com/thrivegoldcoast/">@thrivegoldcoast</a>
     </h2>
-    <br />
+    <br /> */}
     <footer className="footer">
       <div className="container taCenter">
         <span>
-          © Copyright {new Date().getFullYear()} All rights reserved. Crafted by{' '}
-          <a href="https://thriveweb.com.au/">Thrive</a>.
+          © Copyright {new Date().getFullYear()} Tous droits réservés. Site fait par{' '}
+          <a href="https://francaisavecdan.netlify.app/contact/">Dan</a>.
         </span>
       </div>
     </footer>
