8300d0f0f76e663e142d24e164811dd746b3067f
Dan Sylvain <dansylvain84@gmail.com>
2020-09-22 11:06:05 +0200

    googlemap fixed

--- src/components/GoogleMap.js
+++ src/components/GoogleMap.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import GoogleMapReact from 'google-map-react'
 import { MapPin } from 'react-feather'
 
-let mapkey = ''
+let mapkey = 'AIzaSyCH1a_9HzEYpCITSwgV4MN5Sgkn28jNz9Y'
 if (process.env.NETLIFY_MAP_KEY) {
   mapkey = process.env.NETLIFY_MAP_KEY
 }
@@ -10,10 +10,10 @@ if (process.env.NETLIFY_MAP_KEY) {
 class GoogleMap extends Component {
   static defaultProps = {
     center: {
-      lat: -28.0914483,
-      lng: 153.4425208
+      lat: 36.3644648,
+      lng: -6.0617302
     },
-    zoom: 14
+    zoom: 8
   }
 
   render() {
@@ -25,7 +25,7 @@ class GoogleMap extends Component {
           defaultCenter={this.props.center}
           defaultZoom={this.props.zoom}
         >
-          <Marker lat={-28.0914483} lng={153.4425208} text={'Kreyser Avrora'} />
+          <Marker lat={36.3644648} lng={-6.0617302} text={'Kreyser Avrora'} />
         </GoogleMapReact>
       </div>
     )
