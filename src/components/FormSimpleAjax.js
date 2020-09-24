372bb9dd9b6177e2b1631ffe12834b2e5639e4e8
Dan Sylvain <dansylvain84@gmail.com>
2020-09-24 12:06:39 +0200

    retour à la normale

--- src/components/FormSimpleAjax.js
+++ src/components/FormSimpleAjax.js
@@ -66,8 +66,8 @@ class Form extends React.Component {
           name={name}
           action={action}
           onSubmit={this.handleSubmit}
-          data-netlify="true"
-          netlify-recaptcha="true"
+          data-netlify=""
+          netlify-recaptcha=""
         >
           {this.state.alert && (
             <div className="Form--Alert">{this.state.alert}</div>
